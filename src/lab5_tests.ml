27f1ec5decf316da291c8a41820003418475f72a
riellyking <riellyking@gmail.com>
2024-04-08 17:10:04 -0600

    added more lexing tests

--- src/lab5_tests.ml
+++ src/lab5_tests.ml
@@ -31,6 +31,45 @@ let lexer_tests =
       "123.0 - .123",
       Ok([NUMBER(123.0); SUB_OP; NUMBER(0.123); EOF]));
 
+    (Some("floating point equation"),
+      "(123.0 - .123) * (5.0 + 1.2)",
+      Ok([LP_KW; NUMBER(123.000000); SUB_OP; NUMBER(0.123000); RP_KW; MUL_OP; LP_KW; NUMBER(5.000000); ADD_OP; NUMBER(1.200000); RP_KW; EOF]));
+    (Some("boolean equation"),
+      "!(true || !false) && (5 >= 4)",
+      Ok([LOG_NOT_OP; LP_KW; TRUE_KW; LOG_OR_OP; LOG_NOT_OP; FALSE_KW; RP_KW; LOG_AND_OP; LP_KW; NUMBER(5.0); GEQ_OP; NUMBER(4.0); RP_KW; EOF]));
+    (Some("list expression"),
+      "[1; 2; 0b111; 0x1a9D; 0.5]",
+      Ok([LSB_KW; NUMBER(1.0); SEMICOLON_OP; NUMBER(2.0); SEMICOLON_OP; NUMBER(7.0); SEMICOLON_OP; NUMBER(6813.0); SEMICOLON_OP; NUMBER(0.5); RSB_KW; EOF]));
+    (Some("comment"),
+      "// this is a comment",
+      Ok([EOF]));
+    (Some("multi-line comment"),
+      "/*
+      this is a multi-line comment
+      
+      lalalalalalala
+      */",
+      Ok([EOF]));
+    (Some("string assignment"),
+      "let my_string = \"this is a string\";",
+      Ok([LET_KW; IDENT("my_string"); ASSIGN_OP; STRING("this is a string"); SEMICOLON_OP; EOF]));
+    (Some("simple function"),
+      "function my_function(num1, num2) {
+        // this function will add 2 input numbers and print the sum
+        const sum = num1 + num2;
+        console.log(sum);
+      }",
+      Ok([FUNC_KW; IDENT("my_function"); LP_KW; IDENT("num1"); COMMA_OP; IDENT("num2"); RP_KW; LCB_KW; CONST_KW; IDENT("sum"); ASSIGN_OP;
+      IDENT("num1"); ADD_OP; IDENT("num2"); SEMICOLON_OP; CONSOLE_KW; DOT_OP; LOG_KW; LP_KW; IDENT("sum"); RP_KW; SEMICOLON_OP; RCB_KW; EOF]));
+
+    (Some("function call"),
+      "my_function(5, 7);",
+      Ok([IDENT("my_function"); LP_KW; NUMBER(5.0); COMMA_OP; NUMBER(7.0); RP_KW; SEMICOLON_OP; EOF]));
+    (Some("non existent operator"),
+      "
+      let x = 5 % 2;
+      ",
+      Error(Javascript_ast.Lexing_error("Error in '' on line 2 col 12: lexing error\n")));
    ])
 
 let parser_tests =
